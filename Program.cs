fc4ffb9ec3e8fe49c39fe770af0f0aa7f553171d
Timur Zanagar <timur@zanagar.de>
2023-06-20 15:53:32 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using OwnSources.Data;
+using MudBlazor.Services;
+using Wolverine;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseWolverine();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
